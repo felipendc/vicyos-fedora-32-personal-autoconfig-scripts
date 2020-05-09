72e55ee8bc530e4022fb022673267336b90d50b1
felipendc <felipendc10@gmail.com>
2020-05-09 10:24:00 -0300

    Vicyos Script Rolling Update: (Sat 09 May 2020 10:24:00 AM -03)

--- vicyos-fedora-personal-autoconfig.sh
+++ vicyos-fedora-personal-autoconfig.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# First thing first: 
+# Open SOFTWARE app and Go to "Software and Repo MENU" and Enable the repos that will suit my needs! (Google-Chrome/NVIDIA/Linux Firmware) and others! 
+# Disable the AUTOMATIC UPDATES on "UPDATES PREFERENCES MENU"
+
 
 # To update your Fedora: 
 sudo dnf upgrade --refresh -y
@@ -7,10 +11,18 @@ sudo dnf upgrade --refresh -y
 # Install the dnf-plugin-system-upgrade package if it is not currently installed:
 sudo dnf install dnf-plugin-system-upgrade -y
 
-
 # To install NVIDIA drivers:
 sudo dnf install nvidia-driver nvidia-settings -y
 
+# Gree With Envy:
+sudo dnf install gwe -y
+
+# Inkscape:
+sudo dnf install inkscape -y
+
+
+# Install Google Chrome: (Don't forget to enable the Google Chrome Repo on Software Settings)
+sudo dnf install google-chrome-stable -y
 
 
 
@@ -21,8 +33,6 @@ sudo dnf install nvidia-driver nvidia-settings -y
 
 
 
-# Install Google Chrome:
-sudo dnf install google-chrome-stable
 
 
 
