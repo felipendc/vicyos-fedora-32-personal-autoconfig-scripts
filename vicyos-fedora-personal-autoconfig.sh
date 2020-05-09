9042307b85c8e8f8a376ea197bf97ecf8bb72777
felipendc <felipendc10@gmail.com>
2020-05-09 10:04:27 -0300

    Vicyos Script Rolling Update: (Sat 09 May 2020 10:04:27 AM -03)

---
+++ vicyos-fedora-personal-autoconfig.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# To update your Fedora: 
+sudo dnf upgrade --refresh -y
+
+# Install the dnf-plugin-system-upgrade package if it is not currently installed:
+sudo dnf install dnf-plugin-system-upgrade -y
+
+
+# To install NVIDIA drivers:
+sudo dnf install nvidia-driver nvidia-settings -y
+
+
+
+
+
+
+
+
+
+
+
+# Install Google Chrome:
+sudo dnf install google-chrome-stable
+
+
+
+
+
+
+
